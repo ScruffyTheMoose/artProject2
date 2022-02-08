b1e99321b428cd3c572c3d605c0154f379b9f4bd
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-02-08 17:07:00 -0600

    build menu and controls

--- vectorFieldTrack.js
+++ vectorFieldTrack.js
@@ -1,6 +1,7 @@
 // array for storing vectors
 locs = [];
 
+
 /**
  * Setting up the P5 sketch.
  * Canvas dimensions are set, test size is set, and a matrix of vectors is set up for drawing the vector field with random deviance from the x,y grid.
@@ -25,7 +26,15 @@ function setup() {
             locs.push(new p5.Vector(res * j + Math.random() * 50, res * i + Math.random() * 50));
         }
     }
-}
+
+    // popup for when the page is loaded
+    window.alert('\
+        Click anywhere in the window to attract the object. Click-and-Hold to guide the object around. Speed depends on distance between the object and your mouse. When you release, the ball will maintain its trajectory and gradually come to a stop\n\
+        \n\
+        Use WASD to select and change different values for the object.')
+
+} // end setup
+
 
 // Initial (x, y) pos of circle
 circleX = 400;
@@ -33,6 +42,13 @@ circleY = 400;
 
 speedX = 0;
 speedY = 0;
+
+// control key val
+control = 1;
+speed = 50;
+resist = 0.005;
+fade = 0.5;
+
 
 /**
  * Rendering components to the canvas.
@@ -43,20 +59,21 @@ speedY = 0;
  * A circle is drawn to be tracked around the canvas.
  * 
  * Vectors are drawn across the grid with color, width, and magnitude varying based on distance from the tracked object.
- * Vector curl and draw-distance is adjusted live with the sliders.
  */
 function draw() {
 
+    // controlling object movement based on mouse pos
     if (mouseIsPressed) {
 
         // Determining x, y movement speed of object based on relative distance from mouse
-        speedX = -45 * (circleX - mouseX) / maxDist();
-        speedY = -45 * (circleY - mouseY) / maxDist();
+        speedX = -speed * (circleX - mouseX) / maxDist();
+        speedY = -speed * (circleY - mouseY) / maxDist();
 
         // Changing x-axis direction and speed based on mouse distance
         circleX += speedX;
         circleY += speedY;
 
+        // bouncing object off borders
     } else {
 
         if (circleX > windowWidth || circleX < 0) {
@@ -70,6 +87,23 @@ function draw() {
         circleX += speedX;
         circleY += speedY;
 
+    }
+
+    // gradually slowing down the object as it drifts
+    // slightly tends to x-axis
+    slowX = resist;
+    slowY = (windowHeight / windowWidth) * resist;
+
+    if (speedX > 0) {
+        speedX -= slowX;
+    } else {
+        speedX += slowX;
+    }
+
+    if (speedY > 0) {
+        speedY -= slowY;
+    } else {
+        speedY += slowY;
     }
 
     // setting background color to turquiose ish
@@ -88,20 +122,20 @@ function draw() {
          * <M> = <a, b>         position vector of mouse
          * To get new vector pointing towards mouse from <P>, 
          * we build a new vector as:
-         * <H> = <-x + a, -y + b>
+         * <H> = <a - x, b - y>
          * 
          * This is necessary because the origin is in the top left of the screen
          * so we must create a new position vector that points towards to true
          * location of the mouse.
          */
-        let h = new p5.Vector(-locs[k].x + circleX, -locs[k].y + circleY);
+        let h = new p5.Vector(circleX - locs[k].x, circleY - locs[k].y);
 
         // creates an instance specific to the new vector h
         push();
 
         // color of the line is dependent on distance from circle
         // given as a ratio of max distance to current distance
-        let rat = 0.1 + (dist(locs[k].x, locs[k].y, circleX, circleY)) / (maxDist());
+        let rat = 0.1 + (dist(locs[k].x, locs[k].y, circleX, circleY)) / (maxDist() * fade);
         let from = color(240);
         let to = color(50);
         let magnitude = lerpColor(from, to, rat);
@@ -131,29 +165,39 @@ function draw() {
 
     }
 
-    // building object to track
-    fill(0, 20, 148);
+    // rendering object to track
+    fill(0, 105, 148);
     noStroke();
     circle(circleX, circleY, 20);
 
-    // gradually slowing down the object as it drifts
-    // currently tends to x-axis
-    slowX = 0.004;
-    slowY = (windowHeight / windowWidth) * slowX;
+    // rendering display box
+    fill(200);
+    displayX = width - 300;
+    displayY = height - 100;
+    rect(displayX, displayY, 300, 100);
 
-    if (speedX > 0) {
-        speedX -= slowX;
-    } else {
-        speedX += slowX;
+    // rendering display information
+    fill(20);
+    textSize(15);
+    switch (control) {
+        case 0:
+            text("Controlling Speed", displayX, displayY + 15);
+            text("Speed = " + speed, displayX, displayY + 35)
+            break;
+
+        case 1:
+            text("Controlling Resistance", displayX, displayY + 15);
+            text("Resistance = " + resist.toFixed(2), displayX, displayY + 35)
+            break;
+
+        case 2:
+            text("Controlling Fade", displayX, displayY + 15);
+            text("Fade = " + fade.toFixed(2), displayX, displayY + 35)
+            break;
     }
 
-    if (speedY > 0) {
-        speedY -= slowY;
-    } else {
-        speedY += slowY;
-    }
+} // end draw
 
-}
 
 /**
  * Function that returns the max diagonal distance of the frame
@@ -164,4 +208,81 @@ function maxDist() {
     heightSq = height * height;
     return Math.sqrt(widthSq + heightSq);
 
-}
+} // end maxDist
+
+
+/**
+ * Function for reading key inputs
+ * The key inputs will correspond with different control values for speed, resistance, and fade of/around the object.
+ * A and D for selecting the element being controlled
+ * W and S for incrementing or decrementing the element's value
+ */
+function keyPressed() {
+
+    if (keyCode === 65) {
+
+        // decrementing control to min 0
+        if (control > 0) {
+            control--;
+            console.log(control);
+        }
+
+    } else if (keyCode === 68) {
+
+        // incrementing control to max 2
+        if (control < 2) {
+            control++;
+            console.log(control);
+        }
+
+    } else if (keyCode === 87) {
+
+        switch (control) {
+            case 0:
+                speed += 5; // negative because of speed value formula
+                console.log(speed);
+                break;
+
+            case 1:
+                if (resist < 1) {
+                    resist += 0.01;
+                    console.log(resist);
+                }
+                break;
+
+            case 2:
+                if (fade < 1) {
+                    fade += 0.1;
+                    console.log(fade);
+                }
+                break;
+        }
+
+    } else if (keyCode === 83) {
+
+        switch (control) {
+            case 0:
+                speed -= 5;
+                console.log(speed);
+                break;
+
+            case 1:
+                if (resist > 0) {
+                    resist -= 0.01;
+                    console.log(resist);
+                }
+
+                if (resist < 0) {
+                    resist = 0;
+                }
+                break;
+
+            case 2:
+                fade -= 0.1;
+                console.log(fade);
+                break;
+        }
+
+    }
+
+} // end keyPressed
